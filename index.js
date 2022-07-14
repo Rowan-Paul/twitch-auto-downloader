2b8d875a88d8b761455da78b35ebdddf326b13fa
Rowan Paul <contact@rowanpaulflynn.com>
2022-07-14 17:06:02 +0200

    Update gitignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 const fetch = require('node-fetch');
-const { getGitUser, downloadVideoChat } = require('./console');
+const { downloadVideoChat } = require('./console');
 require('dotenv').config();
 
 //TODO: fetch videos from channel
