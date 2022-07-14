42f3b4d122a8f27d08318085f0d90d03e718222b
Rowan Paul <contact@rowanpaulflynn.com>
2022-07-14 18:33:37 +0200

    Switch to node-cmd

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const cron = require('node-cron');
 const fetch = require('node-fetch');
-const { downloadVideoChat } = require('./console');
+const cmd = require('node-cmd');
 require('dotenv').config();
+const fs = require('fs');
 
-//TODO: fetch videos from channel
-//TODO: save video to disk
 //TODO: check if disk has video
 //TODO: add database with logs
 //TODO: display logs somewhere
+//TODO: add ffmpeg path variable
+//TODO: add h265
 async function temp() {
   console.log('Starting...');
   const { access_token } = await getBearerToken();
@@ -19,9 +20,45 @@ async function temp() {
   console.log(`Found ${videos.data.length} streams, downloading now`);
   videos.data.forEach((video) => {
     const vodid = video.url.match(/([0-9]{10})/g)[0];
-    console.log('VOD id: ', vodid);
+    const title = video.title;
+    console.log(`VOD id+title: ${vodid}_${title}`);
 
-    downloadVideoChat(vodid, video.title, (value) => console.log(value));
+    let downloadedFiles = [];
+    fs.readdir('./output', (err, files) => {
+      files.forEach((file) => {
+        downloadedFiles.push(file);
+      });
+    });
+
+    if (!downloadedFiles.includes(`${vodid}_${title}.mp4`)) {
+      cmd
+        .run(
+          `TwitchDownloaderCLI -m VideoDownload --id ${vodid} --ffmpeg-path "ffmpeg.exe" -o "output/${vodid}_${title}.mp4"`,
+          function (err, data, stderr) {
+            console.log('Finished');
+          }
+        )
+        .stdout.on('data', (data) => console.log(data));
+      cmd
+        .run(
+          `TwitchDownloaderCLI -m ChatDownload --id ${vodid} -o "output/${vodid}_chat.json"`,
+          function (err, data, stderr) {
+            console.log('Finished');
+          }
+        )
+        .stdout.on('data', (data) => console.log(data));
+      cmd
+        .run(
+          `TwitchDownloaderCLI -m ChatRender -i "output/${vodid}_chat.json" -h 600 -w 350 --framerate 60 --update-rate 1 -o "output/${vodid}_${title}_chat.mp4"`,
+          function (err, data, stderr) {
+            console.log('Finished');
+          }
+        )
+        .stdout.on('data', (data) => console.log(data));
+    } else {
+      console.log('Stream already downloaded');
+    }
+
     console.log('===');
   });
 }
